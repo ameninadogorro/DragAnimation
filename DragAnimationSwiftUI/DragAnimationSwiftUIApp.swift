3eba56258f540d28b4378f4f561adbaf08d533ad
Ana Guimarães <anaguimaraes@MacBook-Pro-de-Ana.local>
2024-07-31 14:18:13 -0300

    first commit

---
+++ DragAnimationSwiftUI/DragAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAnimationSwiftUIApp.swift
+//  DragAnimationSwiftUI
+//
+//  Created by Ana Guimarães on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DragAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
